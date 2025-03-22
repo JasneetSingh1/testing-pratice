5fee98916a45fe08a95106a3005d1d87bc88dff2
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-03-22 17:54:04 -0600

    feat: create caesarCipher function

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+
 export function capitalize(string) {
   let result = string.replace(string[0], string[0].toUpperCase())
   return result;
@@ -38,15 +39,101 @@ export const Calculator = {
   }
 }
 
+
+
+// FUNCTION 4: CAESAR CIPHER
+
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    let newNode = new Node(value);
+
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  getTail() {
+    return this.tail;
+  }
+  getHead() {
+    return this.head;
+  }
+
+  find(value) {
+    let start = this.head;
+    let count = 0;
+    while (start != null) {
+      if (start.value == value) return count;
+      count++;
+      start = start.next;
+    }
+    return null;
+  }
+
+  getAt(index) {
+    let result = this.head;
+    for (let i = 0; i < index; i++) {
+      if (result != null) {
+        result = result.next;
+      }
+    }
+
+    return result.value;
+  }
+}
+
 export function caeserCipher(str, num) {
   let alphabet = "abcdefghijklmnopqrstuvwxyz";
   let result = "";
-
+  let alpha = new LinkedList();
+  for(let i = 0; i < alphabet.length ; i++){
+    alpha.append(alphabet.charAt(i));
+  }
+  
+  let head = alpha.getHead();
+  let end = alpha.getTail();
+  end.next = head;
   for (let i = 0; i < str.length; i++) {
 
-    const char = (str[i].charCodeAt()) + num;
-    result = result + String.fromCharCode(char);
+    if(/^[A-Za-z]+$/.test(str[i])){
+      const char = str[i];
+      let index = alpha.find(char.toLowerCase());
+      let shifted = alpha.getAt(index + num);
+  
+      if(char == char.toUpperCase()){
+        result = result + shifted.toUpperCase();
+      }
+      else{
+        result = result + shifted.toLowerCase();
+      }
+      
+    }
+
+    else{
+      result = result + str[i];
+    }
+
   }
+
 
   return result;
 }
+
+
+
--- functions.test.js
+++ functions.test.js
@@ -1,71 +1,69 @@
 import {
-    Calculator,
-    capitalize,
-    reverseString,
-    caeserCipher
-
+  Calculator,
+  capitalize,
+  reverseString,
+  caeserCipher,
 } from "./functions";
 
 // FUNCTION 1: CAPITALIZE
-test('Capitalize', () => {
-    expect(capitalize("hello")).toBe("Hello");
+test("Capitalize", () => {
+  expect(capitalize("hello")).toBe("Hello");
 });
 
-
-test('Capitalize: multiple word string', () => {
-    expect(capitalize("dog in")).toBe("Dog in");
+test("Capitalize: multiple word string", () => {
+  expect(capitalize("dog in")).toBe("Dog in");
 });
-
 
 // FUNCTION 2: REVERSESTRING
 
+test("Reverse", () => {
+  expect(reverseString("dog")).toBe("god");
+});
 
-test('Reverse', () => {
-    expect(reverseString("dog")).toBe("god");
-})
+test("Reverse: multiple word string", () => {
+  expect(reverseString("apple in a hole")).toBe("eloh a ni elppa");
+});
 
-test('Reverse: multiple word string', () => {
-    expect(reverseString("apple in a hole")).toBe("eloh a ni elppa");
-})
-
-test('Reverse: multiple word string with Capital', () => {
-    expect(reverseString("Apple in a hole")).toBe("eloh a ni elppA");
-})
-
+test("Reverse: multiple word string with Capital", () => {
+  expect(reverseString("Apple in a hole")).toBe("eloh a ni elppA");
+});
 
 // FUNCTION 3: Calculator
 
-test('Calculator: Add ', () => {
-    expect(Calculator.add(2, 3)).toBe(5);
-})
+test("Calculator: Add ", () => {
+  expect(Calculator.add(2, 3)).toBe(5);
+});
 
-test('Calculator: Add floating numbers ', () => {
-    expect(Calculator.add(.2, .1)).toBeCloseTo(0.3);
-})
+test("Calculator: Add floating numbers ", () => {
+  expect(Calculator.add(0.2, 0.1)).toBeCloseTo(0.3);
+});
 
-test('Calculator: Subtract ', () => {
-    expect(Calculator.subtract(5, 3)).toBe(2);
-})
+test("Calculator: Subtract ", () => {
+  expect(Calculator.subtract(5, 3)).toBe(2);
+});
 
-test('Calculator: Subtract floating numbers ', () => {
-    expect(Calculator.subtract(.5, .3)).toBeCloseTo(.2);
-})
+test("Calculator: Subtract floating numbers ", () => {
+  expect(Calculator.subtract(0.5, 0.3)).toBeCloseTo(0.2);
+});
 
-test('Calculator: Divide ', () => {
-    expect(Calculator.divide(6, 2)).toBe(3);
-})
+test("Calculator: Divide ", () => {
+  expect(Calculator.divide(6, 2)).toBe(3);
+});
 
-test('Calculator: Multiply ', () => {
-    expect(Calculator.multiply(6, 2)).toBe(12);
-})
-
+test("Calculator: Multiply ", () => {
+  expect(Calculator.multiply(6, 2)).toBe(12);
+});
 
 // FUNCTION 4: CAESAR CIPHER
 
-test('Caesar Cipher:', () => {
-    expect(caeserCipher('xyz', 1)).toBe('abc');
-})
+test("Caesar Cipher:", () => {
+  expect(caeserCipher("xyz", 3)).toBe("abc");
+});
 
-test('Caesar Cipher: Case Preservation', () => {
-    expect(caeserCipher('HeLLo', 3)).toBe('KhOOr');
-})
+test("Caesar Cipher: Case Preservation", () => {
+  expect(caeserCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("Caesar Cipher: Punctuation Preservation", () => {
+  expect(caeserCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
